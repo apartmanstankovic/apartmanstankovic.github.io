09551076fbd1fb2a1b513e2c0a2ff34b895c2b6b
Branko Stankovic <76656355+Bane1989@users.noreply.github.com>
2023-05-08 22:38:45 +0200

    exchanged apartment and resort sections

--- website/script.js
+++ website/script.js
@@ -2,7 +2,7 @@
 
 // about us slider
 
-const apartmentImg = document.querySelector(".apartment-img");
+const apartmentImg = document.querySelector(".slide-img");
 const apartmentImages = [
   ["./images/1.jpeg", "./images/2.jpeg", "./images/3.jpg", "./images/4.jpg"],
   ["./images/5.jpg", "./images/6.jpg", "./images/7.jpg", "./images/8.jpg"],
@@ -12,7 +12,7 @@ const apartmentImages = [
 let imgNum = 0;
 
 const next = () => {
-  let parent = event.target.closest(".apartment-slider");
+  let parent = event.target.closest(".img-slider");
   let tabImages = +parent.getAttribute("data-tabIndex");
   imgNum++;
   if (imgNum > apartmentImages[tabImages].length - 1) {
@@ -22,7 +22,7 @@ const next = () => {
 };
 
 const prev = () => {
-  let parent = event.target.closest(".apartment-slider");
+  let parent = event.target.closest(".img-slider");
   let tabImages = +parent.getAttribute("data-tabIndex");
   imgNum--;
   if (imgNum < 0) {
@@ -35,23 +35,22 @@ const prev = () => {
 
 const tabLinks = document.querySelectorAll(".tab-links");
 const tabContents = document.querySelectorAll(".tab-contents");
-const imgSlider = document.querySelector(".apartment-slider");
-const mapSection = document.querySelector(".map")
+const imgSlider = document.querySelector(".img-slider");
+const mapSection = document.querySelector(".map");
 
 tabLinks.forEach((tabLink) => {
   tabLink.addEventListener("click", (e) => {
-    let tabIndex = tabLink.getAttribute("data-tabLink") - 1;
+    let tab = e.target.getAttribute("data-tabLink");
     imgSlider.setAttribute("data-tabIndex", tabIndex);
-    if(tabIndex === 3) {
-      imgSlider.style.display = "none"
-      mapSection.style.display = "block"
-    }
-    else {
-      imgSlider.style.display = "block"
-      mapSection.style.display = "none"
+    if (tab === tabLinks.length) {
+      imgSlider.style.display = "none";
+      mapSection.style.display = "block";
+    } else {
+      imgSlider.style.display = "block";
+      mapSection.style.display = "none";
       imgSlider
-      .querySelector("img")
-      .setAttribute("src", apartmentImages[tabIndex][0]);
+        .querySelector("img")
+        .setAttribute("src", apartmentImages[tabIndex][0]);
     }
 
     for (link of tabLinks) {
@@ -62,64 +61,7 @@ tabLinks.forEach((tabLink) => {
     }
 
     e.target.classList.add("active");
-    let tab = e.target.getAttribute("data-tabLink");
     let tabContent = document.querySelector(`[data-tabContent="${tab}"]`);
     tabContent.classList.add("active");
   });
-});
-
-// full img slider
-
-// const fullImg = document.querySelector(".full-img");
-// const img = document.querySelectorAll(".img");
-// const closeFull = document.querySelector(".close-full");
-// const resortFigures = document.querySelectorAll("figure");
-
-// let imgIndex;
-
-// img.forEach((img) =>
-//   img.addEventListener("click", () => {
-//     let src = img.getAttribute("src");
-//     document.querySelector(".full").style.display = "flex";
-//     fullImg.setAttribute("src", src);
-//     imgIndex = img.parentElement.getAttribute("data-index");
-//   })
-// );
-
-// const incrementImg = () => {
-//   imgIndex++;
-//   if (imgIndex > resortFigures.length) {
-//     imgIndex = 1;
-//   }
-
-//   let imgSrc = document
-//     .querySelector(`[data-index="${imgIndex}"]`)
-//     .querySelector(".rotate-slide")
-//     .getAttribute("src");
-//   fullImg.setAttribute("src", imgSrc);
-// };
-
-// const decrementImg = () => {
-//   imgIndex--;
-//   if (imgIndex < 1) {
-//     imgIndex = resortFigures.length;
-//   }
-//   let imgSrc = document
-//     .querySelector(`[data-index="${imgIndex}"]`)
-//     .querySelector(".rotate-slide")
-//     .getAttribute("src");
-//   fullImg.setAttribute("src", imgSrc);
-// };
-
-// closeFull.addEventListener("click", () => {
-//   document.querySelector(".full").style.display = "none";
-// });
-
-// footer newsletter input
-
-const newsletterInput = document.querySelector(".newsletter-email");
-const newsletterLabel = document.querySelector(".newsletter-label");
-
-newsletterInput.addEventListener("change", (event) => {
-  console.log("radi!!!");
 });
